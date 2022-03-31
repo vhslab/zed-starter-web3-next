e1ccb9067201518429050dd6925a43f26a99f739
holoform <kyermakou@vhslab.com>
2022-03-31 17:52:26 -0400

    [feature] biconomy and segment support

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,14 @@ import CoinbaseWalletCard from '../components/connectors/CoinbaseWalletCard'
 import MetaMaskCard from '../components/connectors/MetaMaskCard'
 import NetworkCard from '../components/connectors/NetworkCard'
 import WalletConnectCard from '../components/connectors/WalletConnectCard'
-import App from "../components/App";
 
 export default function Home() {
   return (
-    <App>
-      <div style={{ display: 'flex', flexFlow: 'wrap', fontFamily: 'sans-serif' }}>
-        <MetaMaskCard />
-        <WalletConnectCard />
-        <CoinbaseWalletCard />
-        <NetworkCard />
-      </div>
-    </App>
+    <div style={{ display: 'flex', flexFlow: 'wrap', fontFamily: 'sans-serif' }}>
+      <MetaMaskCard />
+      <WalletConnectCard />
+      <CoinbaseWalletCard />
+      <NetworkCard />
+    </div>
   )
 }
