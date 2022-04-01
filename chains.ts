11415ccbcf6fb50bb60b217e6057fd4ab022f119
rplusq <rafaplusq@gmail.com>
2022-04-01 02:57:34 -0400

    change: setup only chains we use

--- chains.ts
+++ chains.ts
@@ -1,35 +1,37 @@
-import type { AddEthereumChainParameter } from '@web3-react/types'
+import type { AddEthereumChainParameter } from "@web3-react/types";
 
-const ETH: AddEthereumChainParameter['nativeCurrency'] = {
-  name: 'Ether',
-  symbol: 'ETH',
+const ETH: AddEthereumChainParameter["nativeCurrency"] = {
+  name: "Ether",
+  symbol: "ETH",
   decimals: 18,
-}
+};
 
-const MATIC: AddEthereumChainParameter['nativeCurrency'] = {
-  name: 'Matic',
-  symbol: 'MATIC',
+const MATIC: AddEthereumChainParameter["nativeCurrency"] = {
+  name: "Matic",
+  symbol: "MATIC",
   decimals: 18,
-}
+};
 
 interface BasicChainInformation {
-  urls: string[]
-  name: string
+  urls: string[];
+  name: string;
 }
 
 interface ExtendedChainInformation extends BasicChainInformation {
-  nativeCurrency: AddEthereumChainParameter['nativeCurrency']
-  blockExplorerUrls: AddEthereumChainParameter['blockExplorerUrls']
+  nativeCurrency: AddEthereumChainParameter["nativeCurrency"];
+  blockExplorerUrls: AddEthereumChainParameter["blockExplorerUrls"];
 }
 
 function isExtendedChainInformation(
   chainInformation: BasicChainInformation | ExtendedChainInformation
 ): chainInformation is ExtendedChainInformation {
-  return !!(chainInformation as ExtendedChainInformation).nativeCurrency
+  return !!(chainInformation as ExtendedChainInformation).nativeCurrency;
 }
 
-export function getAddChainParameters(chainId: number): AddEthereumChainParameter | number {
-  const chainInformation = CHAINS[chainId]
+export function getAddChainParameters(
+  chainId: number
+): AddEthereumChainParameter | number {
+  const chainInformation = CHAINS[chainId];
   if (isExtendedChainInformation(chainInformation)) {
     return {
       chainId,
@@ -37,112 +39,67 @@ export function getAddChainParameters(chainId: number): AddEthereumChainParamete
       nativeCurrency: chainInformation.nativeCurrency,
       rpcUrls: chainInformation.urls,
       blockExplorerUrls: chainInformation.blockExplorerUrls,
-    }
+    };
   } else {
-    return chainId
+    return chainId;
   }
 }
 
-export const CHAINS: { [chainId: number]: BasicChainInformation | ExtendedChainInformation } = {
+export const CHAINS: {
+  [chainId: number]: BasicChainInformation | ExtendedChainInformation;
+} = {
   1: {
     urls: [
-      process.env.infuraKey ? `https://mainnet.infura.io/v3/${process.env.infuraKey}` : undefined,
-      process.env.alchemyKey ? `https://eth-mainnet.alchemyapi.io/v2/${process.env.alchemyKey}` : undefined,
-      'https://cloudflare-eth.com',
+      process.env.infuraKey
+        ? `https://mainnet.infura.io/v3/${process.env.infuraKey}`
+        : undefined,
+      process.env.alchemyKey
+        ? `https://eth-mainnet.alchemyapi.io/v2/${process.env.alchemyKey}`
+        : undefined,
+      "https://cloudflare-eth.com",
     ].filter((url) => url !== undefined),
-    name: 'Mainnet',
-  },
-  3: {
-    urls: [process.env.infuraKey ? `https://ropsten.infura.io/v3/${process.env.infuraKey}` : undefined].filter(
-      (url) => url !== undefined
-    ),
-    name: 'Ropsten',
-  },
-  4: {
-    urls: [process.env.infuraKey ? `https://rinkeby.infura.io/v3/${process.env.infuraKey}` : undefined].filter(
-      (url) => url !== undefined
-    ),
-    name: 'Rinkeby',
+    name: "Mainnet",
   },
   5: {
-    urls: [process.env.infuraKey ? `https://goerli.infura.io/v3/${process.env.infuraKey}` : undefined].filter(
-      (url) => url !== undefined
-    ),
-    name: 'Görli',
-  },
-  42: {
-    urls: [process.env.infuraKey ? `https://kovan.infura.io/v3/${process.env.infuraKey}` : undefined].filter(
-      (url) => url !== undefined
-    ),
-    name: 'Kovan',
-  },
-  // Optimism
-  10: {
     urls: [
-      process.env.infuraKey ? `https://optimism-mainnet.infura.io/v3/${process.env.infuraKey}` : undefined,
-      'https://mainnet.optimism.io',
+      process.env.infuraKey
+        ? `https://goerli.infura.io/v3/${process.env.infuraKey}`
+        : undefined,
     ].filter((url) => url !== undefined),
-    name: 'Optimism',
-    nativeCurrency: ETH,
-    blockExplorerUrls: ['https://optimistic.etherscan.io'],
-  },
-  69: {
-    urls: [
-      process.env.infuraKey ? `https://optimism-kovan.infura.io/v3/${process.env.infuraKey}` : undefined,
-      'https://kovan.optimism.io',
-    ].filter((url) => url !== undefined),
-    name: 'Optimism Kovan',
-    nativeCurrency: ETH,
-    blockExplorerUrls: ['https://kovan-optimistic.etherscan.io'],
-  },
-  // Arbitrum
-  42161: {
-    urls: [
-      process.env.infuraKey ? `https://arbitrum-mainnet.infura.io/v3/${process.env.infuraKey}` : undefined,
-      'https://arb1.arbitrum.io/rpc',
-    ].filter((url) => url !== undefined),
-    name: 'Arbitrum One',
-    nativeCurrency: ETH,
-    blockExplorerUrls: ['https://arbiscan.io'],
-  },
-  421611: {
-    urls: [
-      process.env.infuraKey ? `https://arbitrum-rinkeby.infura.io/v3/${process.env.infuraKey}` : undefined,
-      'https://rinkeby.arbitrum.io/rpc',
-    ].filter((url) => url !== undefined),
-    name: 'Arbitrum Testnet',
-    nativeCurrency: ETH,
-    blockExplorerUrls: ['https://testnet.arbiscan.io'],
+    name: "Görli",
   },
   // Polygon
   137: {
     urls: [
-      process.env.infuraKey ? `https://polygon-mainnet.infura.io/v3/${process.env.infuraKey}` : undefined,
-      'https://polygon-rpc.com',
+      process.env.infuraKey
+        ? `https://polygon-mainnet.infura.io/v3/${process.env.infuraKey}`
+        : undefined,
+      "https://polygon-rpc.com",
     ].filter((url) => url !== undefined),
-    name: 'Polygon Mainnet',
+    name: "Polygon Mainnet",
     nativeCurrency: MATIC,
-    blockExplorerUrls: ['https://polygonscan.com'],
+    blockExplorerUrls: ["https://polygonscan.com"],
   },
   80001: {
-    urls: [process.env.infuraKey ? `https://polygon-mumbai.infura.io/v3/${process.env.infuraKey}` : undefined].filter(
-      (url) => url !== undefined
-    ),
-    name: 'Polygon Mumbai',
+    urls: [
+      process.env.infuraKey
+        ? `https://polygon-mumbai.infura.io/v3/${process.env.infuraKey}`
+        : undefined,
+    ].filter((url) => url !== undefined),
+    name: "Polygon Mumbai",
     nativeCurrency: MATIC,
-    blockExplorerUrls: ['https://mumbai.polygonscan.com'],
+    blockExplorerUrls: ["https://mumbai.polygonscan.com"],
   },
-}
+};
 
-export const URLS: { [chainId: number]: string[] } = Object.keys(CHAINS).reduce<{ [chainId: number]: string[] }>(
-  (accumulator, chainId) => {
-    const validURLs: string[] = CHAINS[Number(chainId)].urls
+export const URLS: { [chainId: number]: string[] } = Object.keys(
+  CHAINS
+).reduce<{ [chainId: number]: string[] }>((accumulator, chainId) => {
+  const validURLs: string[] = CHAINS[Number(chainId)].urls;
 
-    if (validURLs.length) {
-      accumulator[Number(chainId)] = validURLs
-    }
+  if (validURLs.length) {
+    accumulator[Number(chainId)] = validURLs;
+  }
 
-    return accumulator
-  },
-  {}
-)
+  return accumulator;
+}, {});
---
+++ util/connectors/chainId.ts
@@ -0,0 +1,1 @@
+export const chainId = Number(process.env.NEXT_PUBLIC_CHAIN_ID) || 80001;
